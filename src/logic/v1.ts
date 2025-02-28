976a8b858237b3834943db46e8dbc2ba509f61d3
Neo2481 <timesmediaworld27@gmail.com>
2025-03-01 01:22:29 +0530

    Update v1.ts

--- src/logic/v1.ts
+++ src/logic/v1.ts
@@ -1,5 +1,3 @@
-
-
 export const M3u8ProxyV1 = async (request: Request<unknown>) => {
   const url = new URL(request.url);
   const refererUrl = decodeURIComponent(url.searchParams.get("referer") || "");
@@ -13,9 +11,6 @@ export const M3u8ProxyV1 = async (request: Request<unknown>) => {
 
   const response = await fetch(targetUrl, {
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
       Referer: refererUrl || "",
       Origin: originUrl || "",
     },
@@ -23,33 +18,31 @@ export const M3u8ProxyV1 = async (request: Request<unknown>) => {
 
   let modifiedM3u8;
   if (targetUrl.includes(".m3u8")) {
-    modifiedM3u8 = await response.text();
-    const targetUrlTrimmed = `${encodeURIComponent(
-      targetUrl.replace(/([^/]+\.m3u8)$/, "").trim()
-    )}`;
+    let content = await response.text();
+    const targetUrlTrimmed = targetUrl.replace(/([^/]+\.m3u8)$/, "").trim();
     const encodedUrl = encodeURIComponent(refererUrl);
     const encodedOrigin = encodeURIComponent(originUrl);
-    modifiedM3u8 = modifiedM3u8.split("\n").map((line) => {
+
+    modifiedM3u8 = content.split("\n").map((line) => {
       if (line.startsWith("#") || line.trim() == '') {
         return line;
-      }
-      else if(proxyAll == 'yes' && line.startsWith('http')){ //https://yourproxy.com/?url=https://somevideo.m3u8&all=yes
+      } else if (proxyAll === "yes" && line.startsWith("http")) {
         return `${url.origin}?url=${line}`;
       }
-      return `?url=${targetUrlTrimmed}${line}${originUrl ? `&origin=${encodedOrigin}` : ""
-      }${refererUrl ? `&referer=${encodedUrl}` : ""
-      }`;
+      return `${targetUrlTrimmed}${line}${originUrl ? `&origin=${encodedOrigin}` : ""}${refererUrl ? `&referer=${encodedUrl}` : ""}`;
     }).join("\n");
   }
 
-  return new Response(modifiedM3u8 || response.body, {
+  const finalContent = modifiedM3u8 || await response.text();
+
+  return new Response(finalContent, {
     status: response.status,
     statusText: response.statusText,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Content-Type":
-        response.headers?.get("Content-Type") ||
-        "application/vnd.apple.mpegurl",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": response.headers.get("Content-Type") || "application/vnd.apple.mpegurl",
     },
   });
-}
+};
